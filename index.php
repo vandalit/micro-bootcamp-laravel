5eb5ff3e930f5d7bb3d2243a066b597632266730
Van <84937597+vandalit@users.noreply.github.com>
2025-08-08 13:29:55 -0400

    index

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+$name = "van";
+$age = 20;
+$isDeveloper = true;
+
+?>
+
+<h1>
+    <?php echo "hola mundo"; ?> <br>
+</h1>
+
+
+<ul>
+    <li><a href="./curso-php/primeros pasos/index.php">primeros pasos php</a></li>
+    <li><a href="./curso-php/next-marvel-movie/index.php">next marvel movie</a></li>
+</ul>
+
+
+<style>
+
+    :root {
+        color-scheme: light dark;
+    }
+    body {
+        display: grid;
+        place-content: center;
+        
+    }
+</style>
