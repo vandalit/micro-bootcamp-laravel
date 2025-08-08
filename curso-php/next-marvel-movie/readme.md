f43cec834ade4ba400c0bd55f22aef2f9b5f3a54
Van <84937597+vandalit@users.noreply.github.com>
2025-08-08 12:59:57 -0400

    init

---
+++ curso-php/next-marvel-movie/readme.md
@@ -0,0 +1,1 @@
+https://github.com/DiljotSG/MCU-Countdown
---
+++ curso-php/primeros pasos/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = "van";
+$age = 20;
+$isDeveloper = true;
+
+?>
+
+
+
+<h1>
+    <?php echo "hola mundo"; ?>
+</h1>
+
+<style>
+
+    :root {
+        color-scheme: light dark;
+    }
+    body {
+        display: grid;
+        place-content: center;
+        
+    }
+</style>
