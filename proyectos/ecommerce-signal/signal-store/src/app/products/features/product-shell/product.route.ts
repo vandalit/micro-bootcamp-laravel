f7a41bcc0bfd761a1b99fe7946d3c0dbb2b51083
Van <84937597+vandalit@users.noreply.github.com>
2025-09-03 22:46:51 -0400

    cart wsf

--- proyectos/ecommerce-signal/signal-store/src/app/products/features/product-shell/product.route.ts
+++ proyectos/ecommerce-signal/signal-store/src/app/products/features/product-shell/product.route.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 
 export default [
     {path: 'products', loadComponent: () => import('../product-list/product-list')},    
-    {path: 'product/:id', loadComponent: () => import('../product-detail/product-detail')}
+    {path: 'product/:id', loadComponent: () => import('../product-detail/product-detail')},
+    {path: 'cart', loadChildren: () => import('../../../cart/cart.route')}
 ] as Routes;
